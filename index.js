8dc82f4019ca0a8ae17014da29c10596ee72ed61
Akshaya <akshayasatheesh@hotmail.co.uk>
2026-03-08 21:32:31 +0000

    WIP: script to automate conventional commits

---
+++ index.js
@@ -0,0 +1,32 @@
+import { input, select, confirm } from "@inquirer/prompts";
+import { typeChoices, emojis } from "./utils.js";
+
+const mainQuestions = {
+  type: await select({
+    message: "What type of commit are you making?",
+    choices: typeChoices,
+    required: true,
+  }),
+  emoji: await select({
+    message: "Choose an emoji that accurately represents the commit:",
+    choices: emojis,
+    required: true,
+  }),
+  description: await input({
+    message: "Write a short description of what the commit does:",
+    required: true,
+  }),
+  confirmation: await confirm({
+    message: "Are you happy with your commit? Please press enter to confirm",
+  }),
+};
+
+const transformOutput = (res) => {
+  return res.confirmation
+    ? `${res.type}(${res.emoji}): ${res.description}`
+    : console.log(
+        "Sorry, something went wrong during the process. Please try again.",
+      );
+};
+
+console.log(transformOutput(mainQuestions));
---
+++ utils.js
@@ -0,0 +1,11 @@
+export const typeChoices = [
+  { name: "fix", value: "fix" },
+  { name: "feat", value: "feat" },
+  { name: "refactor", value: "refactor" },
+  { name: "style", value: "style" },
+  { name: "chore", value: "chore" },
+  { name: "docs", value: "docs" },
+  { name: "test", value: "test" },
+];
+
+export const emojis = ["🪄", "✅", "♻️", "💅", "🧹", "📖", "🧪", "🔥"];
